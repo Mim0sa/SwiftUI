4f009ed1039e95b2bbb9a203ec4bda31129da4a0
mimosa <mimosa@lunaon.com>
2021-02-01 15:31:35 +0800

    add project for SwiftUI thkofdev

---
+++ SwiftUI 编程思想/Demo1/Demo1/Demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo1App.swift
+//  Demo1
+//
+//  Created by Mimosa on 2021/2/1.
+//
+
+import SwiftUI
+
+@main
+struct Demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
